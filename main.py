03e0d9adfa683aaa2eb0b569af04181b14f24d7d
sutariyajay <34575033+sutariyajay@users.noreply.github.com>
2020-09-18 17:20:05 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,43 @@
+from PIL import Image, ImageTk
+import tkinter
+import os
+
+os.chdir("E://Jay//New folder")#directory path
+
+images=os.listdir()
+
+#Importing Photos
+image_list = [img for img in images]
+current = 0
+
+def move(Rd):
+    global current, image_list
+    if not (0 <= current + Rd < len(image_list)):
+        return
+    current += Rd
+    image = Image.open(image_list[current])
+    images=image.resize((500,500),Image.ANTIALIAS)
+    photo = ImageTk.PhotoImage(images)
+
+    label['image'] = photo
+    label.photo = photo
+
+    # label=tkinter.Label(image=photo)
+    # label.pack()
+
+
+root = tkinter.Tk()
+root.geometry("%dx%d+0+0"%(root.winfo_screenwidth(),root.winfo_screenheight()))
+label = tkinter.Label(root, compound=tkinter.TOP)
+label.pack()
+
+frame = tkinter.Frame(root)
+frame.pack()
+
+tkinter.Button(frame, text='Previous', command=lambda: move(-1)).pack(side=tkinter.LEFT)
+tkinter.Button(frame, text='Next', command=lambda: move(+1)).pack(side=tkinter.LEFT)
+
+
+move(0)
+
+root.mainloop()
